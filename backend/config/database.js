25c2bd7a88863717ebff8e6daf1b2ae94a3ec0bb
S Tejeswar <tejeswar.s21@iiits.in>
2025-07-18 21:12:57 +0530

    error handling 1

--- backend/config/database.js
+++ backend/config/database.js
@@ -9,7 +9,7 @@ const connectDB = async (uri) => {
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    // console.error(`Error: ${error.message}`);
+    console.error(`Error: ${error.message}`);
     process.exit(1);
   }
 };
